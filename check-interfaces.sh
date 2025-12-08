556311b62bda9a0098108703e576f9cf50cafe1a
Anthony Biasi <a@biasi.co>
2025-12-07 19:06:00 -0800

    Clean up repository - remove development artifacts

--- check-interfaces.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "=== All Network Interfaces ==="
-ip link show | grep -E "^[0-9]+:" | awk '{print $2}' | tr -d ':'
-echo ""
-echo "=== Interfaces with IPs ==="
-ip addr show | grep -E "^[0-9]+:|inet " | grep -v "127.0.0.1"
